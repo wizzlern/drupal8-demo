63d455d9a80e3d98ae58813e0acdf45d4bf802bd
Erik Stielstra <info@erikstielstra.nl>
2014-03-20 23:39:18 +0100

    Initial commit of Demo Block module.

---
+++ demo_block/lib/Drupal/demo_block/Plugin/Block/DemoBlock.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\demo_block\Plugin\Block\DemoBlock.
+ */
+
+namespace Drupal\demo_block\Plugin\Block;
+
+use Drupal\block\Annotation\Block;
+use Drupal\block\BlockBase;
+use Drupal\Core\Annotation\Translation;
+
+/**
+ * Provides a 'Example: configurable text string' block.
+ *
+ * @Block(
+ *   id = "demo_block",
+ *   subject = @Translation("Demo"),
+ *   admin_label = @Translation("Demo Block")
+ * )
+ */
+class DemoBlock extends BlockBase {
+
+  /**
+   * Overrides \Drupal\block\BlockBase::defaultConfiguration().
+   */
+  public function defaultConfiguration() {
+    return array(
+      'label' => t('Demo'),
+      'content' => t('Default demo content'),
+      'cache' => DRUPAL_CACHE_PER_ROLE,
+    );
+  }
+
+  /**
+   * Overrides \Drupal\block\BlockBase::blockForm().
+   */
+  public function blockForm($form, &$form_state) {
+    $form['content'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Block contents'),
+      '#size' => 60,
+      '#description' => t('This text will appear in the demo block.'),
+      '#default_value' => $this->configuration['content'],
+    );
+    return $form;
+  }
+
+  /**
+   * Overrides \Drupal\block\BlockBase::blockSubmit().
+   */
+  public function blockSubmit($form, &$form_state) {
+    $this->configuration['content'] = $form_state['values']['content'];
+  }
+
+  /**
+   * Implements \Drupal\block\BlockBase::blockBuild().
+   */
+  public function build() {
+    return array(
+      '#markup' => $this->configuration['content'],
+    );
+  }
+
+}
