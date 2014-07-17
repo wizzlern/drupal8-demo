1d99c79f0403997e684139c9e6f7afbe55f82ac7
Erik Stielstra <info@erikstielstra.nl>
2014-07-17 12:15:49 +0200

    Routing: Updated RouteSubscriber::alterRoutes() after change of its signature.

--- demo_routing/src/Routing/RouteSubscriber.php
+++ demo_routing/src/Routing/RouteSubscriber.php
@@ -21,16 +21,14 @@ class RouteSubscriber extends RouteSubscriberBase {
    * alterRoutes() is called at the RoutingEvents::ALTER event using the
    * event subscriber in RouteSubscriberBase.
    */
-  public function alterRoutes(RouteCollection $collection, $provider) {
-    if ($provider == 'user') {
-      // Change path '/user/login' to '/login'.
-      $route = $collection->get('user.login');
-      $route->setPath('/login');
+  public function alterRoutes(RouteCollection $collection) {
+    // Change path '/user/login' to '/login'.
+    $route = $collection->get('user.login');
+    $route->setPath('/login');
 
-      // Always deny access to '/user/logout'.
-      // Note that the second parameter of setRequirement() is a string.
-      $route = $collection->get('user.logout');
-      $route->setRequirement('_access', 'FALSE');
-    }
+    // Always deny access to '/user/logout'.
+    // Note that the second parameter of setRequirement() is a string.
+    $route = $collection->get('user.logout');
+    $route->setRequirement('_access', 'FALSE');
   }
 }
