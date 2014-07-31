d79f134475a3ac822d25382ad4ddc8c0752d5b66
Erik Stielstra <info@erikstielstra.nl>
2014-07-31 15:16:03 +0200

    Plugin: Replaced LinkFormatter plugin by a couple of hooks.

--- demo_plugin/src/Plugin/Field/FieldFormatter/DemoPluginUppercaseFormatter.php
+++ demo_plugin/src/Plugin/Field/FieldFormatter/DemoPluginUppercaseFormatter.php
@@ -29,8 +29,8 @@ class DemoPluginUppercaseFormatter extends FormatterBase {
   public function viewElements(FieldItemListInterface $items) {
     $elements = array();
 
-    // Note that entity output is cached. After any change in the code below the
-    // cache (registry) has to be cleared.
+    // Note that entity output is cached. After changing the code below the
+    // cache (registry) should be cleared or the content should be re-saved.
     foreach ($items as $delta => $item) {
       /** @var \Drupal\text\Plugin\Field\FieldType\TextItem $item */
       $elements[$delta] = array('#markup' => drupal_strtoupper($item->processed));
